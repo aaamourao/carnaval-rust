92a2e49ce8c3042fe2203a9bacc9d4cbde77b05f
Adriano Mourao <mourao.aaa@gmail.com>
2024-02-10 17:59:24 -0300

    stop using _f64

--- src/activation/mod.rs
+++ src/activation/mod.rs
@@ -4,9 +4,9 @@ use std::f64;
 
 pub fn sigm(x: f64) -> f64 {
     let minus_x = -x;
-    1_f64 / (1_f64 + minus_x.exp())
+    1.0 / (1.0 + minus_x.exp())
 }
 
 pub fn relu(x: f64) -> f64 {
-    x.max(0.0_f64)
+    x.max(0.0)
 }
