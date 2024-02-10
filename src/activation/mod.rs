e39d54618982b90504356526cef54ff97e2e3bf7
Adriano Mourao <mourao.aaa@gmail.com>
2024-02-10 17:57:39 -0300

    Fix sigm function and add tests

--- src/activation/mod.rs
+++ src/activation/mod.rs
@@ -3,9 +3,10 @@
 use std::f64;
 
 pub fn sigm(x: f64) -> f64 {
-    1 / (1 + x.exp())
+    let minus_x = -x;
+    1_f64 / (1_f64 + minus_x.exp())
 }
 
 pub fn relu(x: f64) -> f64 {
-    x.max(0)
+    x.max(0.0_f64)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 mod activation;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use activation::{relu, sigm};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn relu_works() {
+        let result = relu(-2.0);
+        let result_bigger_than_0 = relu(3.0);
+        assert_eq!(result, 0.0);
+        assert_eq!(result_bigger_than_0, 3.0);
+    }
+
+    #[test]
+    fn sigm_works() {
+        let result = sigm(0.0);
+        let result_6 = sigm(6.0);
+        let result_minus_6 = sigm(-6.0);
+        assert_eq!(result, 0.5);
+        assert_eq!(result_6, 0.9975273768433653);
+        assert_eq!(result_minus_6, 0.0024726231566347743);
     }
 }
