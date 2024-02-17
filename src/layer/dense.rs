53e4ceddda8f711b027b1d0c0fe934ffac1b78e9
Adriano Mourao <mourao.aaa@gmail.com>
2024-02-17 17:55:28 -0300

    fix Dense layer representation

--- src/layer/dense.rs
+++ src/layer/dense.rs
@@ -1,43 +1,26 @@
-use std::ops::Mul;
-use ndarray::{Array, Ix3};
+use std::ops::{Add, Mul};
+use ndarray::{Array, Ix2};
 use ndarray_rand::RandomExt;
 use rand::distributions::Uniform;
 use crate::activation::ActivationFunctionType;
 use crate::layer::{Layer, LayerType};
 
 pub struct Dense {
-    pub nn_layers: Vec<Array<f64, Ix3>>,
-    pub bias: Vec<Array<f64, Ix3>>,
+    pub layers: Array<f64, Ix2>,
+    pub bias: Array<f64, Ix2>,
     pub activation_function: ActivationFunctionType,
 }
 
 impl Dense {
-    pub fn new(input_dim: (usize, usize, usize),
-           output_height: usize,
+    pub fn new(input_dim: usize,
+           output_dim: usize,
            activation_function: Option<ActivationFunctionType>) -> Self {
-        let (input_depth, input_height, input_width) = input_dim;
 
-        let mut layers_size = if output_height < input_height {
-            input_height - output_height
-        } else {
-            output_height - input_height
-        };
-
-        let mut nn_layers: Vec<Array<f64, Ix3>> = Vec::with_capacity(layers_size);
-
-        // layers size is usize, so we need to increment it in order
-        // to add the correct number of layers
-        layers_size += 1;
-        while layers_size > 0 {
-            nn_layers.push(Array::random((input_depth, input_height, input_width),
-                                         Uniform::new(0.0, 1.0)));
-           layers_size -= 1;
-        }
-
-        let bias = vec![Array::zeros((input_depth, 1, input_width))];
+        let layers = Array::random((input_dim, output_dim), Uniform::new(0.0, 1.0));
+        let bias = Array::random((1, output_dim), Uniform::new(0.0, 1.0));
 
         Dense {
-            nn_layers,
+            layers,
             bias,
             activation_function: activation_function.unwrap_or(ActivationFunctionType::Relu),
         }
@@ -53,13 +36,7 @@ impl Layer for Dense {
         return self.activation_function
     }
 
-    fn forward(&self, input: Array<f64, Ix3>) -> Array<f64, Ix3> {
-        let mut current = input;
-
-        for layer in self.nn_layers.iter() {
-            current = current.mul(layer);
-        }
-
-        return current
+    fn forward(&self, input: Array<f64, Ix2>) -> Array<f64, Ix2> {
+        return self.layers.mul(input).add(&self.bias)
     }
 }
