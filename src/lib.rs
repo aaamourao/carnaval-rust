60be60cc88f10e22f30cccefe1254d89a1745d57
Adriano Mourao <mourao.aaa@gmail.com>
2024-02-18 05:11:59 -0300

    Plot to evaluate dense

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod model;
 mod tests {
     use more_asserts::{assert_ge, assert_le};
     use ndarray::{array};
+    use plotpy::{Curve, Plot};
     use crate::activation::{ActivationFunctionType, relu, sigmoid};
     use crate::layer::Layer;
     use crate::layer::dense::Dense;
@@ -45,12 +46,40 @@ mod tests {
         for weight in &nn.layers {
             println!("weight: {weight}");
         }
-        let inference_result = &nn.forward(array![[[1.], [1.]]]);
+        let inference_result = &nn.forward(&array![[[1.], [1.]]]);
 
         if inference_result.is_ok() {
             for (row, value) in inference_result.as_ref().unwrap().iter().enumerate() {
                 println!["value multiplied by 1.0 + bias({:?}): {value}", nn.bias[[0, row, 0]]]
             }
         }
+    }
+    
+    #[test]
+    fn dense_plot() {
+        let nn = Dense::new(5, 5, Some(ActivationFunctionType::Sigmoid));
+        for weight in &nn.layers {
+            println!("weight: {weight}");
+        }
+
+        let x = array![[[2.0], [4.0], [6.0], [8.0], [10.0]]];
+        let y_result = &nn.forward(&x);
+        let y = match y_result {
+            Ok(result) => result,
+            _ => panic!["Forward returned error"]
+        };
+
+        let mut curve = Curve::new();
+        curve.set_label("y = nn.forward(x)");
+        curve.draw(&x.into_raw_vec(), &y.clone().into_raw_vec());
+
+        let mut plot = Plot::new();
+        plot.set_title("NN with 5 hidden layers");
+        plot.set_subplot(1, 1, 1)
+            .set_title("Dense network test")
+            .add(&curve)
+            .grid_labels_legend("x", "y");
+
+        let result = plot.save("plots/test.svg");
     }
 }
